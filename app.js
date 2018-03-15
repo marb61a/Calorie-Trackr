79d5f0b99164dd5928191480517423788668a2c8
martin byrne <marb61a@gmail.com>
2018-03-15 12:10:57 +0000

    added initial app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// Storage Controller
+
+
+// Item Controller
+// Function uses the IIFE pattern
+const ItemCtrl = (function(){
+    // Item Constructor
+    const Item = function(id, name, calories){
+        
+    };
+    
+})();
+
+
+// Ui Controller
+// Function uses the IIFE pattern
+const UICtrl = (function(){
+    
+})();
+
+// App Controller
+// Function uses the IIFE pattern
+const App = (function(ItemCtrl, UICtrl){
+    
+})(ItemCtrl, UICtrl);
