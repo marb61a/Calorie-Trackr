49e2439b6d40db911572221dff1b147e7f29643f
martin byrne <marb61a@gmail.com>
2018-03-26 11:48:06 +0000

    added item clearing and hiding to app js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const ItemCtrl = (function(){
     // Data Structure, similar to state in React applications
     const data = {
         items: [
-            {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 1, name: 'Chocolate', calories: 300},
-            {id: 2, name: 'Eggs', calories: 400}
+            // {id: 0, name: 'Steak Dinner', calories: 1200},
+            // {id: 1, name: 'Chocolate', calories: 300},
+            // {id: 2, name: 'Eggs', calories: 400}
             
         ],
         currentItem: null,
@@ -93,6 +93,9 @@ const UICtrl = (function(){
             }; 
         },
         addListItem: function(item){
+            // Show items
+            document.querySelector(UISelectors.itemList).style.display= 'block';
+            
             // Create an li element
             const li = document.createElement('li');
             
@@ -112,7 +115,11 @@ const UICtrl = (function(){
             document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
         },
         clearInput: function(){
-            
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
+        },
+        hideList: function(){
+            document.querySelector(UISelectors.itemList).style.display = 'none';    
         },
         getSelectors: function(){
             return UISelectors;
@@ -160,11 +167,17 @@ const App = (function(ItemCtrl, UICtrl){
             // Fetches items from the data structure
             const items = ItemCtrl.getItems();
             
-            // Populate list with items
-            UICtrl.populateItemList(items);
+            // Check if any items
+            if(items.length === 0){
+                UICtrl.hideList();
+            } else {
+                // Populate list with items
+                UICtrl.populateItemList(items);
+            }
             
             // Load event listeners
             loadEventListeners();
+            
         }
     };
     
